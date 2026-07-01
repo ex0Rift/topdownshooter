51400634780ed21cad0d8cbcb9845a99cb70c9d5
Nate <daviesnathanael45@gmail.com>
2026-07-01 10:13:08 +0100

    reworked enemy movment and started intergrated abulity for player to shoot

--- public/script.js
+++ public/script.js
@@ -3,22 +3,37 @@ const ctx = gameScreen.getContext('2d');
 
 let player = {x: 100, y: 100, size: 50, speed: 10, health: 400};
 let activeEnemies = [];
+let bullets = [];
 
 let flagStartWave = false;
 let wave = 0;
 
+let mouse = {x:null, y:null};
+
 const enemiesAvailable = 
 {
-    basicSlime: {size:20, speed:0.001, health:50, x:null, y:null},
-    midSlime:   {size:40, speed:0.01, health:100, x:null, y:null}
+    basicSlime: {size:20, speed:2, health:50, x:null, y:null},
+    midSlime:   {size:40, speed:3, health:100, x:null, y:null},
+    largeSlime: {size:60, speed:1, health:200, x:null, y:null},
+    fastSlime:  {size:30, speed:8, health:40, x:null, y:null},
+    tanksSlime: {size:100, speed:0.8, health:300, x:null, y:null}
 };
 const enemyList = Object.values(enemiesAvailable);
 
 
-//keypress 
+//keypress checking 
 const keys = {};
 document.addEventListener('keydown', e => keys[e.key] = true);
 document.addEventListener('keyup', e => keys[e.key] = false);
+
+//mouse checking
+const mousePressed = false;
+document.addEventListener('mousedown', e => mousePressed = true);
+document.addEventListener('mouseup', e => mousePressed = false);
+document.addEventListener('mousemove', e => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+});
 
 function resizeScreen(){
     const rect = gameScreen.getBoundingClientRect();
@@ -59,11 +74,25 @@ function update(){
 
     if (keys['z'])flagStartWave = true;
 
+    //shoot player gun
+    if (mousePressed)
+    {
+
+    }
+
     //move the enemies
     for (let i = 0; i < activeEnemies.length; i++)
     {
-        activeEnemies[i].x += (player.x - activeEnemies[i].x) * activeEnemies[i].speed;
-        activeEnemies[i].y += (player.y - activeEnemies[i].y) * activeEnemies[i].speed;
+        //find the difference between the two points
+        const dx = player.x - activeEnemies[i].x;
+        const dy = player.y - activeEnemies[i].y;
+        //pythag to find the distance between the two points
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        //prevent division by 0
+        if (distance > player.size/2){
+            activeEnemies[i].x += (dx / distance) * activeEnemies[i].speed;
+            activeEnemies[i].y += (dy / distance) * activeEnemies[i].speed;
+        }
     }
 }
 
@@ -94,6 +123,12 @@ function render(){
     ctx.fillStyle = "#ffffff";
     ctx.fillRect(player.x, player.y, player.size,player.size);
     
+    //draw the bullets
+    for (let i = 0; i < bullets.length; i++)
+    {
+
+    }
+
     //draw the enemies
     for (let i = 0; i < activeEnemies.length; i++)
     {
