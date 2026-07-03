9fb004c05fea8f0d8892133c5491ee2d1f9f7f48
Nate <daviesnathanael45@gmail.com>
2026-07-03 23:08:42 +0100

    collision now works between player and enemies, health gets decreased by a costom ammunt when enemy collides with player. no the player cant die yet im still going

--- public/script.js
+++ public/script.js
@@ -1,7 +1,9 @@
 const gameScreen = document.getElementById('game');
 const ctx = gameScreen.getContext('2d');
 
-let player = {x: 100, y: 100, size: 50, speed: 10, health: 400};
+let player = {x: 100, y: 100, size: 18, speed: 10, health: 400, damageCoolDown: 120};
+const maxdamageCoolDown = player.damageCoolDown;
+
 let activeEnemies = [];
 let activeBullets = [];
 
@@ -14,21 +16,21 @@ const bulletsAvailable = {
     Pistol:  {interval:10, damage:19,spread:1,x:null,y:null,dx:null,dy:null,distance:null},
     SMG:     {interval:2, damage:12,spread:1,x:null,y:null,dx:null,dy:null,distance:null},
     Rifle:   {interval:20, damage:50,spread:1,x:null,y:null,dx:null,dy:null,distance:null},
-    DeathRay:{interval:1, damage:200,spread:1,x:null,y:null,dx:null,dy:null,distance:null},
+    DeathRay:{interval:1, damage:200,spread:75,x:null,y:null,dx:null,dy:null,distance:null},
     Shotgun :{interval:15, damage:15,spread:5,x:null,y:null,dx:null,dy:null,distance:null},
 };
 const bulletList = Object.values(bulletsAvailable);
-let currentBulletType = 4;
+let currentBulletType = 1;
 let currentBulletInterval = 0;
 const bulletSpeed = 50;
 
 const enemiesAvailable = 
 {
-    basicSlime: {size:20, speed:2, health:50, x:null, y:null},
-    midSlime:   {size:40, speed:3, health:100, x:null, y:null},
-    largeSlime: {size:60, speed:1, health:200, x:null, y:null},
-    fastSlime:  {size:30, speed:8, health:40, x:null, y:null},
-    tanksSlime: {size:100, speed:0.8, health:300, x:null, y:null}
+    basicSlime: {size:20, speed:2, health:50, damage:20, x:null, y:null},
+    midSlime:   {size:40, speed:3, health:100, damage:25, x:null, y:null},
+    largeSlime: {size:60, speed:1, health:200, damage:35, x:null, y:null},
+    fastSlime:  {size:30, speed:8, health:40, damage:18, x:null, y:null},
+    tanksSlime: {size:100, speed:0.8, health:300, damage:100, x:null, y:null}
 };
 const enemyList = Object.values(enemiesAvailable);
 
@@ -46,6 +48,18 @@ document.addEventListener('mousemove', e => {
     mouse.x = e.clientX;
     mouse.y = e.clientY;
 });
+
+const checkPointInPerimiter = (px, py, x1, y1, x2, y2) => {
+    //checking a single point (px,py) to see if it is inside a perimeter
+    //perimmeter is top left (x1,y1) and top right (x2,y2)
+    if(px < x2 && px > x1 && py < y2 && py > y1){
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 function resizeScreen(){
     const rect = gameScreen.getBoundingClientRect();
@@ -113,7 +127,6 @@ function update(){
                 //dx and dy with applied roation
                 activeBullets.at(-1).dx = dx * cos - dy * sin;
                 activeBullets.at(-1).dy = dx * sin + dy * cos;
-
                 //work out distance from mouse at begining for direction
                 activeBullets.at(-1).distance = Math.sqrt(activeBullets.at(-1).dx * activeBullets.at(-1).dx + activeBullets.at(-1).dy * activeBullets.at(-1).dy);
             }
@@ -121,8 +134,7 @@ function update(){
     }
     //itterates bullet delay
     if (currentBulletInterval != 0)currentBulletInterval --;
-
-    //move the enemies
+    //move the enemies and check collision
     for (let i = 0; i < activeEnemies.length; i++)
     {
         //find the difference between the two points
@@ -135,12 +147,42 @@ function update(){
             activeEnemies[i].x += (dx / distance) * activeEnemies[i].speed;
             activeEnemies[i].y += (dy / distance) * activeEnemies[i].speed;
         }
+        //collision for interacting with player
+        //
+
+        //check if middle of player is inside bounding box of the current itteration of enemy
+        const collide = checkPointInPerimiter(
+            player.x+(player.size/2),
+            player.y+(player.size/2),
+            activeEnemies[i].x,activeEnemies[i].y,
+            activeEnemies[i].x+activeEnemies[i].size,
+            activeEnemies[i].y+activeEnemies[i].size
+        );
+        //check if damadge cooldown is over AND is colliding
+        if (player.damageCoolDown == 0 && collide == 1)
+        {
+            //remove enemey aproriate amount of health
+            changePlayerHealth(-activeEnemies[i].damage);
+            //reset cooldown
+            player.damageCoolDown = maxdamageCoolDown;
+        }
+        //itterate cooldown
+        if(player.damageCoolDown > 0)player.damageCoolDown --;
     }
     //move the bullets
     for (let i = 0; i < activeBullets.length; i++)
-    {
+    {   
         activeBullets[i].x += (activeBullets[i].dx / activeBullets[i].distance) * bulletSpeed;
         activeBullets[i].y += (activeBullets[i].dy / activeBullets[i].distance) * bulletSpeed;
+        //check if position os outside of the window
+        if 
+        (
+            activeBullets[i].x < 0 || activeBullets[i].x > gameScreen.width ||
+            activeBullets[i].y < 0 || activeBullets[i].y > gameScreen.height
+        ){  
+            //delete bullet if outside the window
+            activeBullets.splice(i,1);
+        }
     }
 }
 
